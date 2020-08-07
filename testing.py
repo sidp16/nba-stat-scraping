f149bbdf2c2b5d26c6718c555d1e49911db873cc
sidp16 <siddharth.prasad1609@gmail.com>
2020-08-07 17:01:08 +0100

    can draw linear regression model with 2 vars

--- testing.py
+++ testing.py
@@ -1,10 +1,16 @@
 import pandas as pd
+import numpy as np
+import requests
+from matplotlib import pyplot as plt
+from sklearn import linear_model
 from nba_api.stats.endpoints import playergamelog
 from nba_api.stats.static import players
 from nba_api.stats.library.parameters import SeasonAll
+from nba_api.stats.endpoints.shotchartdetail import ShotChartDetail
+from nba_api.stats import endpoints
 
 class NBAPlayer:
-    def __init__(self, fullname, season):
+    def __init__(self, fullname, season=0):
         self.fullname = fullname
         self.season = season
 
@@ -41,11 +47,57 @@ class NBAPlayer:
             return careerlog
         except:
             return "N/A"
-        
-player = NBAPlayer('James Harden', '2017')
 
-details = player.details()
-gamelog = player.seasonLog()
-careerlog = player.careerlog()
+data = endpoints.leagueleaders.LeagueLeaders()
+leaders = data.league_leaders.get_data_frame()
 
+x, y = leaders.FGA/leaders.GP, leaders.PTS/leaders.GP
 
+x = np.array(x).reshape(-1,1)     
+y = np.array(y).reshape(-1,1)   
+
+model = linear_model.LinearRegression()    
+model.fit(x,y)                             
+
+r2 = round(model.score(x,y), 2)            
+predicted_y = model.predict(x) 
+
+plt.scatter(x, y, s=15, alpha=.5)                            
+plt.plot(x, predicted_y, color = 'black')                    
+plt.title('Relationship Between FGA and PPG')          
+plt.xlabel('FGA per Game')                                   
+plt.ylabel('Points Per Game') 
+
+player1 = NBAPlayer(leaders.PLAYER[0])
+player1d = player1.details()                              
+
+plt.annotate(player1d['last_name'],                       
+              (x[0], y[0]),                       
+              (x[0]-6,y[0]-2),                    
+              arrowprops=dict(arrowstyle='-'))
+
+player2 = NBAPlayer(leaders.PLAYER[7])
+player2d = player2.details()
+
+plt.annotate(player2d['first_name'],
+             (x[7], y[7]),
+             (x[7]-7, y[7]+3),
+             arrowprops=dict(arrowstyle='-'))
+
+player3 = NBAPlayer(leaders.PLAYER[1])
+player3d = player3.details()
+
+plt.annotate(player3d['first_name'],
+             (x[1], y[1]),
+             (x[1], y[1]-10),
+             arrowprops=dict(arrowstyle='-'))
+
+player4 = NBAPlayer(leaders.PLAYER[13])
+player4d = player4.details()
+
+plt.annotate(player4d['first_name'],
+             (x[13], y[13]),
+             (x[13]-3, y[13]-11),
+             arrowprops=dict(arrowstyle='-'))
+
+plt.savefig('graph2.png', dpi=300)
