78bb4ffc67b4a19f921e77add7d419639b0abef2
sidp16 <siddharth.prasad1609@gmail.com>
2020-08-07 12:47:22 +0100

    api working with spyder, can see game logs

--- testing.py
+++ testing.py
@@ -1,5 +1,9 @@
+from nba_api.stats.endpoints import playergamelog
 from nba_api.stats.static import players
 
 players_dict = players.get_players()
 
-bron = [player for player in players_dict if player['full name'] == 'LeBron James'][0]
+curry = [player for player in players_dict if player['full_name'] == 'Stephen Curry'][0]
+
+findingCurryGames = playergamelog.PlayerGameLog(player_id='201939', season='2015')
+curryGameLog = findingCurryGames.get_data_frames()[0]
