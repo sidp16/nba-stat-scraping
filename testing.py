ea66f52f435b4235731217b268fc309521e7fa5e
sidp16 <siddharth.prasad1609@gmail.com>
2020-08-07 14:40:24 +0100

    player class to see some data

--- testing.py
+++ testing.py
@@ -1,9 +1,39 @@
+import pandas as pd
 from nba_api.stats.endpoints import playergamelog
 from nba_api.stats.static import players
 
-players_dict = players.get_players()
+class NBAPlayer:
+    def __init__(self, fullname, season, status=None):
+        self.fullname = fullname
+        self.season = season
 
-curry = [player for player in players_dict if player['full_name'] == 'Stephen Curry'][0]
+    def details(self):
+        try:
+            players_dict = players.get_players()
+            player_details = [player for player in players_dict if player['full_name'].lower() == self.fullname.strip().lower()][0]
+            
+            return player_details
+        except:
+            return "N/A"
 
-findingCurryGames = playergamelog.PlayerGameLog(player_id='201939', season='2015')
-curryGameLog = findingCurryGames.get_data_frames()[0]
+    def gameLog(self):
+        try:
+            if self.season < 1900:
+                    return "N/A"
+            
+            playerDetails = self.details()
+         
+            findGames = playergamelog.PlayerGameLog(player_id=playerDetails['id'], season=self.season.strip())
+            gamelog = findGames.get_data_frames()[0]
+
+            return gamelog
+        except:
+            return "N/A"
+        
+        
+curry = NBAPlayer('lebron james ', '1016')
+
+details = curry.details()
+gamelog = curry.gameLog()
+
+
